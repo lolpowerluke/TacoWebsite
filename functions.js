cdaa711c8695605d13208f37566af7a6f33f588b
lolpowerluke <47672899+lolpowerluke@users.noreply.github.com>
2023-05-30 16:40:31 +0200

    added month view (partly finished)

--- functions.js
+++ functions.js
@@ -11,3 +11,46 @@ function signUp() {
 async function createAccount(username, password) {
   const response = await fetch('http://jurnas.synology.me:7070/tacoapi-TacoAPI.0.3/Account/create?username=' + username + '&password=' + password);
 }
+function showCalendarMonth() {
+  let nav = 0;
+  document.getElementById("calendar").innerHTML = "";
+  for(let i = 0; i < 6; i++) {
+    let weekElement = document.createElement('div');
+    document.getElementById("calendar").appendChild(weekElement);
+    weekElement.id = "week";
+    weekElement.classList.add("week" + i);
+    for(let j = 0; j < 7; j++) {
+      let dayElement = document.createElement('div');
+      weekElement.appendChild(dayElement);
+      dayElement.id = "day";
+      dayElement.classList.add("day" + (j+1))
+      if(i==0) {
+        let spanElement = document.createElement('span');
+        dayElement.appendChild(spanElement);
+        switch(j) {
+          case 0:
+            spanElement.innerHTML = "Monday";
+            break;
+          case 1:
+            spanElement.innerHTML = "Tuesday";
+            break;
+          case 2:
+            spanElement.innerHTML = "Wednasday";
+            break;
+          case 3:
+            spanElement.innerHTML = "Thursday";
+            break;
+          case 4:
+            spanElement.innerHTML = "Friday";
+            break;
+          case 5:
+            spanElement.innerHTML = "Saturday";
+            break;
+          case 6:
+            spanElement.innerHTML = "Sunday";
+            break;
+        }
+      }
+    }
+  }
+}
